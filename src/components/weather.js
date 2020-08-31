58c01b5f901f0f80cefe6841fedae4affc97d7e5
Supriya <supriyagalbale@gmail.com>
2020-08-31 11:18:54 +0530

    setting up project

---
+++ src/components/weather.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Weather = () => {
+  return (
+    <div style={styles.weather} className="Weather">
+      <h1>London</h1>
+    </div>
+  );
+}
+
+const styles = {
+    weather : {
+    }
+}
+
+export default Weather;
