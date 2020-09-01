34bcf16126ff220253800475a826d685fca86e12
Supriya <supriyagalbale@gmail.com>
2020-09-01 12:58:31 +0530

    adding images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Weather from './components/weather'
+import Weather from './components/weather';
+import './App.css';
 
 function App() {
   return (
--- src/components/weather.js
+++ src/components/weather.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 const Weather = () => {
   return (
-    <div style={styles.weather} className="Weather">
-      <h1>London</h1>
+    <div style={styles.weather} className="container">
+      <div className="cards">
+        <h1>London</h1>
+        <div><img src= './sun.png' alt="cloudy"></img></div>
+
+      </div>
+      
     </div>
   );
 }
 
-const styles = {
-    weather : {
-    }
-}
+
 
 export default Weather;
